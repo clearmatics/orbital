a3920aa83bb218065836290bcb6617b22734a8bd
Duarte Aragao <daragao@gmail.com>
2017-10-13 17:18:30 +0100

    added simple test for random contract input

--- main_test.go
+++ main_test.go
@@ -37,3 +37,23 @@ func Test_Process_MissingMessage_ReturnSuccess(t *testing.T) {
 	assert.NotEqual(t, nil, processError)
 	assert.Equal(t, []byte(nil), outputBuffer)
 }
+
+func Test_Process_GenInputs(t *testing.T) {
+    // generate key ring
+    n := 4
+    ring, _,sks := GenerateRandomRing(n)
+
+    // message hexadecimal string to bytes
+    rawMessage := "50b44f86159783db5092ebe77fb4b9cc29e445e54db17f0e8d2bed4eb63126fc"
+    message := hexString2Bytes(rawMessage)
+
+    // generate signature and smart contract withdraw and deposit input data
+    signature, _ := ProcessSignature(ring,sks,message)
+
+    // verify signature
+    verif := true
+    for i := 0; i < len(signature); i++ {
+        verif = verif && RingVerif(ring, message, signature[i])
+    }
+    assert.True(t,verif)
+}
