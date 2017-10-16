68f9a1b0c528b2182d49abc73d63a11c757d29bf
George Ornbo <go@clearmatics.com>
2017-10-16 11:35:44 +0100

    Extend test to exercise Ctlist loop

--- ringsignature_test.go
+++ ringsignature_test.go
@@ -5,13 +5,16 @@
 package main
 
 import (
+	"math/big"
 	"testing"
 )
 
 func TestRingSignatureString(t *testing.T) {
-	r := RingSignature{}
+	r := RingSignature{
+		Ctlist: make([]*big.Int, 1),
+	}
 	actual := r.String()
-	expected := "tau: X: <nil>, Y: <nil>\nctlist: [\n]\n"
+	expected := "tau: X: <nil>, Y: <nil>\nctlist: [\n\t<nil>\n]\n"
 	if actual != expected {
 		t.Errorf("Expected %v but got %v", expected, actual)
 	}
