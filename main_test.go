6d3587f7307b36b1b020c0c63a15c702634f42de
Duarte Aragao <daragao@gmail.com>
2017-10-17 14:23:09 +0100

    removed tests using process()

--- main_test.go
+++ main_test.go
@@ -5,39 +5,6 @@ import (
 	"testing"
 )
 
-func Test_Process_MissingPrivateKeys_ReturnSuccess(t *testing.T) {
-	privateKeysFile := "fakeFile.json"
-	publicKeysFile := "data/pubkeys.json"
-	rawMessage := "1234567890"
-
-	outputBuffer, processError := Process(privateKeysFile, publicKeysFile, rawMessage)
-
-	assert.NotEqual(t, nil, processError)
-	assert.Equal(t, []byte(nil), outputBuffer)
-}
-
-func Test_Process_MissingPublicKeys_ReturnSuccess(t *testing.T) {
-	privateKeysFile := "data/privkeys.json"
-	publicKeysFile := "fakeFile.json"
-	rawMessage := "1234567890"
-
-	outputBuffer, processError := Process(privateKeysFile, publicKeysFile, rawMessage)
-
-	assert.NotEqual(t, nil, processError)
-	assert.Equal(t, []byte(nil), outputBuffer)
-}
-
-func Test_Process_MissingMessage_ReturnSuccess(t *testing.T) {
-	privateKeysFile := "data/privkeys.json"
-	publicKeysFile := "fakeFile.json"
-	rawMessage := ""
-
-	outputBuffer, processError := Process(privateKeysFile, publicKeysFile, rawMessage)
-
-	assert.NotEqual(t, nil, processError)
-	assert.Equal(t, []byte(nil), outputBuffer)
-}
-
 func Test_Process_GenInputs(t *testing.T) {
 	// generate key ring
 	n := 4
